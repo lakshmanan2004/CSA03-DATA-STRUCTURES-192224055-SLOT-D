b66389fcd1b42c773bf836b19e447dbd6c6c5822
lakshmanan2004 <124753145+lakshmanan2004@users.noreply.github.com>
2023-08-29 14:06:24 +0530

    Create Bubble Sort.c

---
+++ Bubble Sort.c
@@ -0,0 +1,29 @@
+//bubble sort
+#include <stdio.h>
+int main()
+{
+    int a[10],i,j,l,temp;
+    printf("enter the no.of elements:");
+    scanf("%d",&l);
+    printf("\n");
+    for(i=0;i<l;i++)
+    {
+        printf("enter the element a[%d]:",i);
+        scanf("%d",&a[i]);
+    }
+    printf("\nafter sorting\n");
+    for(i=0;i<l;i++)
+    {
+        for(j=i+1;j<l;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
